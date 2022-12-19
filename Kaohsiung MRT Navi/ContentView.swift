4b7be93824fb78d13fe53ed8796c1cb87861b7d2
Tun-Kai Yang <ks97121@gmail.com>
2022-12-19 22:47:31 +0800

    Initial Commit

---
+++ Kaohsiung MRT Navi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kaohsiung MRT Navi
+//
+//  Created by Yang Tun-Kai on 2022/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kaohsiung MRT Navi/Kaohsiung_MRT_NaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kaohsiung_MRT_NaviApp.swift
+//  Kaohsiung MRT Navi
+//
+//  Created by Yang Tun-Kai on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct Kaohsiung_MRT_NaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
